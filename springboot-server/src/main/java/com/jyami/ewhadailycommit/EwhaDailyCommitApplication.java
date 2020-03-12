ae1b249de60a66ed65cb72f5ed9b90b9ea54dd1b
mjung1798 <mor2222@naver.com>
2020-03-12 23:09:06 +0900

    Feat: daily commit springboot api ver

---
+++ springboot-server/src/main/java/com/jyami/ewhadailycommit/EwhaDailyCommitApplication.java
@@ -0,0 +1,13 @@
+package com.jyami.ewhadailycommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwhaDailyCommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwhaDailyCommitApplication.class, args);
+    }
+
+}
